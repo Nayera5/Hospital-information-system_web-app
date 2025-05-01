07ad41951a3af5455058b51d3c5adff29178bcfb
Nada Hesham <149081533+Nada-Hesham249@users.noreply.github.com>
2025-05-02 02:21:15 +0300

    Add files via upload

--- frontend/components/Login.js
+++ frontend/components/Login.js
@@ -1,9 +1,12 @@
+
 import React, { useState } from 'react';
 import { MdEmail } from "react-icons/md";
 import { RiLockPasswordLine } from "react-icons/ri";
 import { Form, InputGroup, Button, Row, Col, Container } from 'react-bootstrap'
 import { Link , useNavigate} from 'react-router-dom';
 import { FaHome } from 'react-icons/fa';
+import photo from '../pictures/doct.jpg'
+
 
 import axios from 'axios';
 import './Signup.css';
@@ -38,75 +41,67 @@ const handleSubmit = async (e) => {
 };
 
   return (
-    <Container fluid className="signin-page">
+    <div className="signup-page">
+      
+      <div className="left-side" style={{
+    backgroundImage: `url(${photo})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  }}>
+      <>
+  <Link to="/" className="home-icon">
+    <FaHome />
+  </Link>
+</>
+      </div>
+      <div className="right-side">
+        <div className="form-content">
+          
 
-       <>
-        <Link to="/" className="home-icon">
-          <FaHome />
-        </Link>
-      </>
-      <Row className="h-100">
-        {/* Left side - Image */}
-        <Col md={6} className="left-side">
-          <img 
-            src="nw.jpeg" 
-            alt="login visual" 
-            className="img-fluid"
-          />
-        </Col>
 
-        {/* Right side - Form */}
-        <Col md={6} className="right-side">
-          <Form onSubmit={handleSubmit} className="p-4 rounded shadow" style={{ minWidth: "300px", background: "white" }}>
-            <h3 className="text-center mb-4" id='login'>Log In</h3>
+            <Form onSubmit={handleSubmit}>
+                        <Form.Group className="mb-3">
+                        <h2 className="text-center mb-4" id='signup'>Log In</h2>
+                        </Form.Group>
+            
+              <>
+               
 
-            <Form.Group className="mb-3">
-              <InputGroup>
-                <InputGroup.Text>
-                  <MdEmail />
-                </InputGroup.Text>
-                <Form.Control
-                  type="email"
-                  name="email"
-                  placeholder="Enter Email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  required
-                />
-              </InputGroup>
-            </Form.Group>
+                <Form.Group className="mb-3">
+                  <InputGroup>
+                    <InputGroup.Text><MdEmail /></InputGroup.Text>
+                    <Form.Control type="email"name="email"placeholder="Email"value={formData.email}onChange={handleChange} required/>
+                  </InputGroup>
+                </Form.Group>
 
-            <Form.Group className="mb-3">
-              <InputGroup>
-                <InputGroup.Text>
-                  <RiLockPasswordLine />
-                </InputGroup.Text>
-                <Form.Control
-                  type="password"
-                  name="password"
-                  placeholder="Enter Password"
-                  value={formData.password}
-                  onChange={handleChange}
-                  required
-                />
-              </InputGroup>
-            </Form.Group>
+                <Form.Group className="mb-3">
+                  <InputGroup>
+                    <InputGroup.Text><RiLockPasswordLine /></InputGroup.Text>
+                    <Form.Control type="password"name="password"placeholder="Password"value={formData.password}onChange={handleChange} required/>
+                  </InputGroup>
+                </Form.Group>
 
-            <div className="d-grid">
-              <Button type="submit" variant="primary">
-                Log In
-              </Button>
-            </div>
-            <div className="mt-3 text-center">
-            <span>Don't have an account? </span>
-            <Link to="/signup" className="signup-link" style={{ textDecoration: "none", fontWeight: "bold" }}>
-                Sign up
-            </Link>
-            </div>
+                
+
+                <Button type="submit" className="w-100 custom-button mt-3">
+                  Log In
+                </Button>
+
+                <div className="text-center mt-3">
+                  <span>Don't have an account? </span>
+                  <Link to="/signup" className="signup-link" style={{ textDecoration: "none", fontWeight: "bold" }}>
+                  Sign Up 
+                  </Link>
+                </div>
+
+
+              </>
+              
+            
           </Form>
-        </Col>
-      </Row>
-    </Container>
+        </div>
+      </div>
+    </div>
   );
 };
 

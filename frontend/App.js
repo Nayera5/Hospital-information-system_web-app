b9eda101299dfe8d4cb56c54dac1986e8a05ee92
Nada Hesham <149081533+Nada-Hesham249@users.noreply.github.com>
2025-05-01 05:13:35 +0300

    Add files via upload

---
+++ frontend/App.js
@@ -0,0 +1,37 @@
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { useState } from "react";
+import "./App.css";
+import Login from "./component/Login";
+import Signup from "./component/Signup";
+import Bar from "./component/Navbar";
+import Home from "./component/Homepage";
+
+function AppContent() {
+  const location = useLocation();
+  const [isSignedUp, setIsSignedUp] = useState(false);
+
+  // إخفاء الـ Navbar في صفحتي signup و login
+  const showNavbar = !["/signup", "/login"].includes(location.pathname);
+
+  return (
+    <>
+      {showNavbar && <Bar isSignedUp={isSignedUp} />}
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<Login setIsSignedUp={setIsSignedUp} />} />
+        <Route path="/signup" element={<Signup setIsSignedUp={setIsSignedUp} />} />
+        <Route path="/profile" element={<h1>Profile Page</h1>} />
+      </Routes>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <AppContent />
+    </Router>
+  );
+}
+
+export default App;
